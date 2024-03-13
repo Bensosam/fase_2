67f02592eb217d27530e28b5170d38fb645d1dd0
37956293 <bryandaniel56789@gmai.com>
2024-03-12 22:18:21 -0600

    se_ingreso_el_metodo_CRUD

--- Persona.cpp
+++ Persona.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 using namespace std;
-/* esto es la clase persona
-que heredara a las clases cliente y empleado 
-*/
-class Persona{
-	// Atributos : nombres,apellidos,direccion,telefono,fecha_nacimiento
-	protected : string nombres,apellidos,direccion,fecha_nacimiento;
-	int telefono;
-	
-	// constructor
-	Persona (){
-	}
-	
-	Persona(string nom,string ape,string dir,int tel,string fn){
-		nombres = nom;
-		apellidos = ape;
-		direccion = dir;
-		telefono = tel;
-		fecha_nacimiento = fn;
-	}
-	// metodos crud = crear,leer,actualizar,borrar
-	void crear();
-	void leer();
-	void actualizar();
-	void borrar(); 
 
+class Persona {
+protected:
+    string nombres, apellidos, direccion, fecha_nacimiento;
+    int telefono;
+
+public:
+    Persona() {}
+    Persona(string nom, string ape, string dir, int tel, string fn)
+        : nombres(nom), apellidos(ape), direccion(dir), telefono(tel), fecha_nacimiento(fn) {}
+
+    void crear() {
+        char respuesta;
+        cout << "Se ha ingresado exitosamente el metodo";
+        cin >> respuesta;
+    }
+
+    void leer() {}
+
+    void actualizar() {}
+
+    void borrar() {}
 };
+
+int main() {
+    Persona p;
+    p.crear(); // Example of using the crear method
+    return 0;
+}
